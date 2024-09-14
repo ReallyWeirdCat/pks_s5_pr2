e8c892de89420dff8d3b2251f7fc6508c9d7cb76
Nikolai Papin <nikolai@weirdcat.ru>
2024-09-14 19:10:42 +0300

    Colors, sizes and fonts adjusted

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'PKS S5P2',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home: const HomePage(),
@@ -36,15 +36,15 @@ class HomePage extends StatelessWidget {
 
            // Название
            const Text(
-               "Авторизация", style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+               "Авторизация", style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900),
            ),
 
            const Spacer(),
 
            Padding(
-             padding: const EdgeInsets.all(15.0),
+             padding: const EdgeInsets.all(20.0),
              child: Wrap(
-               runSpacing: 15.0,
+               runSpacing: 20.0,
                alignment: WrapAlignment.spaceAround,
                children: <Widget> [
                  // Поле логина
@@ -52,7 +52,7 @@ class HomePage extends StatelessWidget {
                    decoration: InputDecoration( hintText: 'Логин',
                      filled: true,
                      fillColor: Colors.black12,
-                     hintStyle: TextStyle(color: Colors.black38, fontSize: 22),
+                     hintStyle: TextStyle(color: Colors.black38, fontSize: 18, fontWeight: FontWeight.w500),
                      enabledBorder: OutlineInputBorder(
                        borderRadius: BorderRadius.all(Radius.circular(5)),
                        borderSide: BorderSide.none,
@@ -69,7 +69,7 @@ class HomePage extends StatelessWidget {
                    decoration: InputDecoration( hintText: 'Пароль',
                      filled: true,
                      fillColor: Colors.black12,
-                     hintStyle: TextStyle(color: Colors.black38, fontSize: 22),
+                     hintStyle: TextStyle(color: Colors.black38, fontSize: 18, fontWeight: FontWeight.w500),
                      enabledBorder: OutlineInputBorder(
                        borderRadius: BorderRadius.all(Radius.circular(5)),
                        borderSide: BorderSide.none,
@@ -87,12 +87,12 @@ class HomePage extends StatelessWidget {
                      Checkbox(value: false, onChanged: (value) {},),
                      TextButton(
                          onPressed: (){},
-                         child: const Text("Запомнить меня", style: TextStyle(color: Colors.grey, fontSize: 20),
+                         child: const Text("Запомнить меня", style: TextStyle(color: Colors.black38, fontSize: 18, fontWeight: FontWeight.w500),
                          )
                      ),
                    ],
                  ),
-
+                 const Spacer(),
                  // Кнопка входа
                  SizedBox(
                    width: double.infinity,
@@ -101,7 +101,7 @@ class HomePage extends StatelessWidget {
                        foregroundColor: Colors.white,
                        backgroundColor: Colors.blue,
                        padding: const EdgeInsets.all(20),
-                       textStyle: const TextStyle(fontSize: 22),
+                       textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                        shape: RoundedRectangleBorder(
                          borderRadius: BorderRadius.circular(5),
                        ),
@@ -116,10 +116,12 @@ class HomePage extends StatelessWidget {
                    width: double.infinity,
                    child: ElevatedButton(
                      style: ElevatedButton.styleFrom(
-                       foregroundColor: Colors.white,
-                       backgroundColor: Colors.blue,
+                       foregroundColor: Colors.blue,
+                       backgroundColor: Colors.white,
+                       side: const BorderSide(color: Colors.blue),
+                       shadowColor: Colors.white,
                        padding: const EdgeInsets.all(20),
-                       textStyle: const TextStyle(fontSize: 22),
+                       textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                        shape: RoundedRectangleBorder(
                          borderRadius: BorderRadius.circular(5),
                        ),
@@ -132,7 +134,7 @@ class HomePage extends StatelessWidget {
                  // Кнопка восстановления
                  TextButton(
                      onPressed: (){},
-                     child: const Text("Забыли пароль?", style: TextStyle(color: Colors.grey, fontSize: 20),
+                     child: const Text("Забыли пароль?", style: TextStyle(color: Colors.grey, fontSize: 18, fontWeight: FontWeight.w500),
                      ),
                  ),
              ]),
